6f7006f1741d442d1684bb7997d141d0121c0c35
nniicc <descriptionboxbelow@gmail.com>
2017-10-24 09:24:37 +0200

    Complete Gulp sprites automation part 3/3

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 svgSprite = require('gulp-svg-sprite'),
-rename = require('gulp-rename');
+rename = require('gulp-rename'),
+del = require('del');
 
 // The svgSprite package require its options to be defined in an object LITERAL, hence the variable using curly brackets. We must then tell svgSprite which MODE it should use
 
@@ -17,7 +18,12 @@ var config = {
   }
 }
 
-gulp.task('createSprite', function(){
+// To delete a folder with this task we leverage the "del" npm package
+gulp.task('beginClean', function() {
+  return del(['./app/temp/sprite/', './app/assets/images/sprites']);
+});
+
+gulp.task('createSprite', ['beginClean'], function(){
   // writing /**/*.svg accounts for any potential future subfolders of ICONS
   // and subfiles with the .svg extension
   return gulp.src('./app/assets/images/icons/**/*.svg')
@@ -31,11 +37,15 @@ gulp.task('copySpriteGraphic', ['createSprite'], function(){
 });
 
 // To have Gulp rename the file, we leverage the gulp-rename package
-// the second argument is a dependency. This task won't run before its dependecies have run
+// the second argument between brackets is a dependency. This task won't run before its dependecies have run
 gulp.task('copySpriteCSS', ['createSprite'], function(){
   return gulp.src('./app/temp/sprite/css/*.css')
     .pipe(rename('_sprite.css'))
     .pipe(gulp.dest('./app/assets/styles/modules'));
 });
 
-gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function () {
+  return del('./app/temp/sprite');
+});
+
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
