6f27e3e09c2189ed173385de6e9634071f2d3133
nniicc <descriptionboxbelow@gmail.com>
2017-10-24 08:21:35 +0200

    Complete Gulp sprites automation part 2/3

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp'),
-svgSprite = require('gulp-svg-sprite');
+svgSprite = require('gulp-svg-sprite'),
+rename = require('gulp-rename');
 
 // The svgSprite package require its options to be defined in an object LITERAL, hence the variable using curly brackets. We must then tell svgSprite which MODE it should use
 
 var config = {
   mode: {
     css: {
+      sprite: 'sprite.svg',
       render: {
         css: {
           template: './gulp/templates/sprite.css'
@@ -22,3 +24,18 @@ gulp.task('createSprite', function(){
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprite/'));
 });
+
+gulp.task('copySpriteGraphic', ['createSprite'], function(){
+  return gulp.src('./app/temp/sprite/css/**/*.svg')
+    .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+// To have Gulp rename the file, we leverage the gulp-rename package
+// the second argument is a dependency. This task won't run before its dependecies have run
+gulp.task('copySpriteCSS', ['createSprite'], function(){
+  return gulp.src('./app/temp/sprite/css/*.css')
+    .pipe(rename('_sprite.css'))
+    .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
