9135affcb818026f28fab69a7622bfc9c2559716
nniicc <descriptionboxbelow@gmail.com>
2017-10-27 08:56:26 +0200

    Style header for desktop & mobile

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,13 +5,14 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
 // the "return" is needed due to gulp.src() being an asynchronous function
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
   //postcss expect an ARRAY of postcss filters !
-    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
     .on('error', function(errorInfo){
       console.log(errorInfo.toString());
       this.emit('end');
